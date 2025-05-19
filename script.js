a73425c51e8155d42d4612783d67b063ae5d028f
NitinLakhera <nitin.lakhera01@gmail.com>
2025-05-19 15:10:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function launchTool(toolName) {
+  alert(toolName + ' tool will launch soon!');
+  // Replace the alert with actual navigation or functionality
+  // Example: window.location.href = 'tools/pdf-to-image.html';
+}
